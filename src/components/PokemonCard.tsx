a5a21b1a7ee98b7bb51ec364933a05a1a50cbe1c
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-27 23:36:00 +0900

    fix: 攻撃の名前を表示した

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,13 +1,19 @@
 import React, { FC } from 'react'
-import { Pokemon } from '../generated/graphql'
+import { Pokemon, Attack } from '../generated/graphql'
 
-export const PokemonCard: FC<Pokemon | any> = ({pokemon}) => {
+export const PokemonCard: FC<Pokemon | any> = ({ pokemon }) => {
   return (
     <div>
+      <p>{pokemon.number}</p>
       <p>{pokemon.name}</p>
       <p>{pokemon.maxHP}</p>
       <p>{pokemon.maxCP}</p>
       <img src={pokemon.image} alt={pokemon.name} />
+      <p>
+        {pokemon.attacks.special.slice(0, 3).map((attack: Attack) => (
+          <span key={`${attack.name}-${attack.damage}`}>{attack.name}</span>
+        ))}
+      </p>
     </div>
   )
 }
