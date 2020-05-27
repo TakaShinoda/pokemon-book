74086db24709a5012a4fafb59848819cf2d4b28d
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-27 23:36:51 +0900

    fix: ボタンをクリックして図版番号9版以降も取得可能にした

--- src/containers/PokemonsContainer.tsx
+++ src/containers/PokemonsContainer.tsx
@@ -1,16 +1,18 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { PokemonCard } from '../components/PokemonCard'
 import { GET_POKEMONS } from '../graphql/get-pokemon'
-import { Pokemon, Query } from '../generated/graphql'
+import { Pokemon } from '../generated/graphql'
 
 export const PokemonsContainer: FC = () => {
+  const [num, setNum] = useState(9)
   const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
-    variables: { first: 9 },
+    variables: { first: num },
   })
 
   return (
     <div className="container">
+      <button onClick={() => setNum((prevNum) => (prevNum + 9))}>more</button>
       {pokemons &&
         pokemons.map((pokemon: Pokemon) => (
           <PokemonCard key={pokemon.id} pokemon={pokemon} />
