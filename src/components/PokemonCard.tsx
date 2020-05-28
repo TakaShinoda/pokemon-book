51257e71581590880561a57d1a922955ebe29ef8
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-28 23:05:08 +0900

    fix: スタイルを修正

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,14 +1,30 @@
 import React, { FC } from 'react'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
 import { Pokemon, Attack } from '../generated/graphql'
 
+const useStyle = makeStyles(() =>
+  createStyles({
+    main: {
+      textAlign: 'center',
+    },
+    image: {
+      marginLeft: '1%',
+      marginRight: '1%',
+      padding: '30px',
+      minHeight: '300px',
+    }
+  })
+)
+
 export const PokemonCard: FC<Pokemon | any> = ({ pokemon }) => {
+  const classes = useStyle()
   return (
     <div>
       <p>{pokemon.number}</p>
       <p>{pokemon.name}</p>
       <p>{pokemon.maxHP}</p>
       <p>{pokemon.maxCP}</p>
-      <img src={pokemon.image} alt={pokemon.name} />
+      <img className={classes.image} src={pokemon.image} alt={pokemon.name} />
       <p>
         {pokemon.attacks.special.slice(0, 3).map((attack: Attack) => (
           <span key={`${attack.name}-${attack.damage}`}>{attack.name}</span>
--- src/containers/PokemonsContainer.tsx
+++ src/containers/PokemonsContainer.tsx
@@ -1,22 +1,42 @@
 import React, { FC, useState } from 'react'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
 import { useQuery } from '@apollo/react-hooks'
 import { PokemonCard } from '../components/PokemonCard'
 import { GET_POKEMONS } from '../graphql/get-pokemon'
 import { Pokemon } from '../generated/graphql'
 
+const useStyle = makeStyles(() =>
+  createStyles({
+    main: {
+      textAlign: 'center',
+    },
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      width: '90%',
+      textAlign: 'center',
+      marginTop: '2%',
+      padding: '20px',
+    }
+  })
+)
+
 export const PokemonsContainer: FC = () => {
   const [num, setNum] = useState(9)
+  const classes = useStyle()
   const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
     variables: { first: num },
   })
 
   return (
-    <div className="container">
-      <button onClick={() => setNum((prevNum) => (prevNum + 9))}>more</button>
-      {pokemons &&
-        pokemons.map((pokemon: Pokemon) => (
-          <PokemonCard key={pokemon.id} pokemon={pokemon} />
-        ))}
-    </div>
+    <>
+      <button onClick={() => setNum((prevNum) => prevNum + 9)}>more</button>
+      <div className={classes.root}>
+        {pokemons &&
+          pokemons.map((pokemon: Pokemon) => (
+            <PokemonCard key={pokemon.id} pokemon={pokemon} />
+          ))}
+      </div>
+    </>
   )
 }
